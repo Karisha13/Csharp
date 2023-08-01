97ed993a7495257a6bbdc1207ad51006d46b97ea
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-01 21:49:09 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("число не трёхзначное");
+}
