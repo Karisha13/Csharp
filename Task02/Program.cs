9c47f12b16b86e37c417401713102cc2eea6dc56
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-02 16:36:16 +0300

    Task02.1

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,9 +3,11 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 > number2)
 {
-    Console.WriteLine(number1);
+    Console.WriteLine("Большее число: " + number1);
+    Console.WriteLine("Меньшее число: " + number2);
 }
 else 
 {
-    Console.WriteLine(number2);
+    Console.WriteLine("Большее число: " + number2);
+    Console.WriteLine("Меньшее число: " + number1);
 }
