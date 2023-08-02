c0d226008e39548520de50ba13bbd6a71fce88f9
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-02 16:33:00 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введи два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else 
+{
+    Console.WriteLine(number2);
+}
