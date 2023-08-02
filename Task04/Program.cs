d273f905b90079827d189db7a38c620214dedc42
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-02 17:10:25 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введи три числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine("Максимальное: " + number1);
+}
+
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine("Максимальное: " + number2);
+}
+
+else if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine("Максимальное: " + number3);
+}
