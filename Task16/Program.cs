e87a7e71bc4b7a12f18b75aab02d05907735d48c
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-05 22:50:44 +0300

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введи два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int kvadrat = number1 * number1;
+if(number2 == kvadrat)
+{
+    Console.WriteLine($"Число {number2} является квадратом {number1}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} не является квадратом {number1}");
+}
