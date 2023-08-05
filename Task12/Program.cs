8246d02212241ef3601fcf62aa45349d28ade089
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-05 22:08:06 +0300

    Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 % number2 == 0)
+{
+    Console.WriteLine($"Число {number1} кратно {number2}");
+}
+else
+{
+    int number3 = number1 % number2;
+    Console.WriteLine($"Остаток равен {number3}");
+}
