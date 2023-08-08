e60bc071a973eb5042b6e6e8674353fe6c3fe45a
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-08 22:33:27 +0300

    Task10

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿ //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является
+ // ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+Console.WriteLine("Введи число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 8 && number > 5)
+{
+    Console.WriteLine("Да");
+}
+
+if (number < 6 && number > 0)
+{
+    Console.WriteLine("Нет");
+}
+
+if (number < 1 && number > 7)
+{
+    Console.WriteLine("Такого дня нет");
+}
--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,10 +1,16 @@
-﻿//Напишите программу, которая принимает на вход трёхзначное число и 
-//на выходе показывает вторую цифру этого числа.
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 //456 -> 5
 //782 -> 8
 //918 -> 1
 Console.WriteLine("Введите трёхзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int result1 = (number / 10);
-int result2 = (result1%10);
-Console.WriteLine(result2);
+if (number >=100 && number <1000) 
+{
+     int result1 = (number / 10);
+     int result2 = (result1%10);
+     Console.WriteLine(result2);
+} 
+else
+{
+     Console.WriteLine("Число не трёхзначное");
+}
