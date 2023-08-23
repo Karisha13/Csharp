cf34866b42de5b58291a8a0423d1f24722fe4cab
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-23 22:21:05 +0300

    22

---
+++ Task22/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа на вход принимает число (N) и выдаёт таблицу квадратов чисел от 1 до N
+System.Console.WriteLine("Введи число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+if(numberN < 0)
+{
+    numberN = numberN * (-1);
+}
+for (int i = 1; i <= numberN; i++)
+{
+    result = Math.Pow(i,2);
+    System.Console.WriteLine(result);
+}
