f7e1080ba42139b80e15e81fb819de9c70f7ac77
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-23 21:58:16 +0300

    17

---
+++ Task17/Program.cs
@@ -0,0 +1,20 @@
+﻿// найти в какой четверти координаты
+Console.WriteLine("Введи Х и Y вашей точки: ");
+int userX = Convert.ToInt32(Console.ReadLine());
+int userY = Convert.ToInt32(Console.ReadLine());
+if(userX > 0 && userY > 0)
+{
+    Console.WriteLine($"Точка [{userX}:{userY}] находится в I Четверти");
+}
+else if(userX < 0 && userY > 0)
+{
+    Console.WriteLine($"Точка [{userX}:{userY}] находится в II Четверти");
+}
+else if(userX < 0 && userY < 0)
+{
+    Console.WriteLine($"Точка [{userX}:{userY}] находится в III Четверти");
+}
+else if(userX > 0 && userY < 0)
+{
+    Console.WriteLine($"Точка [{userX}:{userY}] находится в IV Четверти");
+}
