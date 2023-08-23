cc4d7c4a14ed45ded675a445dd50cf65ebc665e8
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-23 22:37:46 +0300

    18

---
+++ Task18/Program.cs
@@ -0,0 +1,23 @@
+﻿//показывает возможный диапазон в заданной четверти координат
+System.Console.WriteLine("Введите № четверти: ");
+int numberCh = Convert.ToInt32(Console.ReadLine());
+if (numberCh == 1)
+{
+    Console.WriteLine("Все X > 0, все Y > 0");
+}
+else if (numberCh == 2)
+{
+    Console.WriteLine("Все X < 0, все Y > 0");
+}
+else if (numberCh == 3)
+{
+    Console.WriteLine("Все X < 0, все Y < 0");
+}
+if (numberCh == 4)
+{
+    Console.WriteLine("Все X > 0, все Y < 0");
+}
+if (numberCh > 4)
+{
+    Console.WriteLine("Такой четверти нет.");
+}
