bffef273813eb61e29284853c2a53e15d69a4477
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-28 20:42:08 +0300

    19

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,5 +1,23 @@
-﻿Console.WriteLine("Введи число: ");
+﻿/* Console.WriteLine("Введи число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int result = number * number; 
-Console.WriteLine(result);
+Console.WriteLine(result); */
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+//digit = number1 % 10;
+//System.Console.WriteLine(digit);
+ 
+
+
+
+  digit = number1 % 10;
+    number1 = number1 / 10;
+    number2 = number2 * 10;
+    number2 = number2 + digit;
+}
+/* 
+int number1 = Convert.ToInt32(Console.ReadLine());
+number1 = number1 / 10;
+System.Console.WriteLine(number1); */
---
+++ Task19/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет,
+ является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+System.Console.WriteLine("Введи пятизначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int num = number1;
+if(number1 < 10000 && number1 > 99999)
+{
+    System.Console.WriteLine("Число не пятизначное.");
+}
+int digit = 0;
+int number2 = 0;
+while (num > 0)
+{
+    digit = num % 10;
+    number2 = number2 * 10 + digit;
+    num = num / 10;
+}
+
+/* System.Console.WriteLine(number2); */
+if (number1 == number2)
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Число не является палиндромом");
+}
+
+
+
