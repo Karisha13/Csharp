37e49e1a501caa5cdc54d6a88d351d304e68e990
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-29 18:43:28 +0300

    24

---
+++ Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿//программа принимает на вход число х и выдаёт сумму чисел от 1 до х
+/* System.Console.WriteLine("Введи число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for(int i = 1; i <= userNumber; i++)
+{
+    sum += i;
+}
+System.Console.WriteLine(sum);
+ */
+
+System.Console.WriteLine("Введи число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    for(int i = 1; i <= userNumber; i++)
+    {
+    sum += i;
+    }
+
+    return sum;
+}
+System.Console.WriteLine($"Сумма цифр в до {userNumber} равна {SumOfNumber(userNumber)}");
