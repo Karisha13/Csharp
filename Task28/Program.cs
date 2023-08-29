c39f1697357643d00bacd77bd776085430641189
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-29 20:06:08 +0300

    28

---
+++ Task28/Program.cs
@@ -0,0 +1,14 @@
+﻿/* программа на вход принимает число х и выдаёт производное от 1 до х*/
+System.Console.WriteLine("Введи число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int ProductOfNumber(int number)
+{
+    int product = 1;
+    while (number != 0)
+    {
+        product *= number;
+        number--;
+    }
+    return product;
+}
+System.Console.WriteLine(ProductOfNumber(userNumber));
