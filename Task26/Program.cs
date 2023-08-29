52883c6c4828e55ed5911f0b0dc6a75e259a0afe
Karina <nickitina.ewgenia13@yandex.ru>
2023-08-29 18:58:21 +0300

    26

---
+++ Task26/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа на вход принимает число и выдаёт количество цифр в числе
+System.Console.WriteLine("Введи число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+System.Console.WriteLine(Quantity(userNumber));
