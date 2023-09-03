33ae5766a07fdf90f2d1208f60fcbe7c4e33f491
Karina <nickitina.ewgenia13@yandex.ru>
2023-09-03 14:16:09 +0300

    41

---
+++ Task41/Program.cs
@@ -0,0 +1,9 @@
+﻿/*пользоваетль вводит числа, а программа показывает сколько из них больше 0*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
