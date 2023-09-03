76140c66149ab55a6efe35905f634a1f18142f85
Karina <nickitina.ewgenia13@yandex.ru>
2023-09-03 12:52:25 +0300

    45

---
+++ Task45/Program.cs
@@ -0,0 +1,30 @@
+﻿/*программа создаёт копию заданного одномерного массива с помощью поэлементного копирование*/
+
+int[] array = {1, 9, 3, 4, 5};
+
+int[] CopyArray(int[] array)
+{
+    int[] arrayL = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayL[i] = array[i];
+    }
+    return arrayL;
+}
+
+int[] userArray = CopyArray(array);
+
+void PrintArray(int[] arrayToPrint)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        System.Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            System.Console.Write(", ");
+        }
+    }
+    System.Console.Write("]");
+}
+PrintArray(userArray);
