cbe656e664103c3343233f8b8098cba13ffec888
Karina <nickitina.ewgenia13@yandex.ru>
2023-09-03 11:52:00 +0300

    42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿/*программа переобразовывает десетичное число в двоичное  3 = 11, 2 = 10, 45 = */
+
+System.Console.Write("Введи число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string Delenie(int a)
+{
+    string result = string.Empty;
+    int temp = 0;
+    for (int i = a; i > 0; i /= 2)
+    {
+        temp = i % 2;
+        result = temp + result;
+    }
+    return result;
+}
+
+string b = Delenie(a);
+System.Console.WriteLine(b);
