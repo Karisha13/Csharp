83f5dedccf2f072f3797601f9b2f910c11be6dc3
Karina <nickitina.ewgenia13@yandex.ru>
2023-09-06 18:53:02 +0300

    46

---
+++ Task46/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными целыми числами.*/
+
+int[,] GetRandom2DArray(int rowNumber, int colNumber, int deviation)//создаёт двумерный массив
+{
+    int[,] result = new int[rowNumber, colNumber];
+    for (int i = 0; i < rowNumber; i++)
+    {
+        for (int j = 0; j < colNumber; j++)
+        {
+            result[i, j] = new Random().Next(-deviation, deviation);
+        }
+    }
+    return result;
+}
+ 
+ void Print2DArray(int[,] arrayToPrint)//выводит индекс (с16-35)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] randomArray = GetRandom2DArray(5, 5, 10);
+Print2DArray(randomArray);
+
